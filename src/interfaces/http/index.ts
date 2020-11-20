800832c687a2140afa25fe441ea9712e647db0af
Ilyas D Jayanto <ell@petaniweb.com>
2020-11-20 20:49:28 +0700

    add cors lib

--- src/interfaces/http/index.ts
+++ src/interfaces/http/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { log } from '../../helper/logger';
 import verificationBiz from '../../domain/verification';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
